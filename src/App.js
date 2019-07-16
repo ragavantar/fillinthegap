cc87fbed24ffd94713903d73087102d31ebeb47f
Hariraagavanthar C R <ragavantar@gmail.com>
2019-07-16 11:47:24 +0530

    added blocks animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  state = { 
+    current: 0,
+    blocks: [ 1, 4,5],
+    filled: Array(5).fill(0)
+   }
+  
+  componentDidMount(){
+    // setInterval(() => {
+    //   let {current} = this.state;
+    //   current++;
+    //   this.setState({current})
+    // }, 1000);
+  }
+
+  toggleBlock = ind => {
+    const { filled } = this.state;
+    filled[ind] = !filled[ind];
+    this.setState({filled}); 
+  }
+
+  render() { 
+    const {current, blocks} = this.state;
+    return ( 
+      <div className="board">
+        {
+          [...Array(10)].map((e, i)=>{
+            return (
+
+            i===current?
+            <div className='row fill'> 
+                {[...Array(6)].map((e, i)=>
+                <div 
+                className={ blocks.includes(i)?'block cell': 'cell' }
+                ></div>)}
+                {[...Array(1)].map((e, i)=>
+                <div 
+                className={ blocks.includes(i)?'block cell': 'cell'}
+                onClick={()=>this.toggleBlock(i)}
+                ></div>)}
+           </div>
+           :
+           <div className='row'> 
+                {[...Array(7)].map((e, i)=><div className="cell"></div>)}
+          </div>
+
+            )
+          })
+        }
+      </div>
+     );
+  }
+}
+ 
 export default App;
